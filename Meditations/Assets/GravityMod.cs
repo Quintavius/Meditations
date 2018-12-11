a621012a4c8e6288280870d4dcf1e93860b856aa
Quintavius <loths.marc@gmail.com>
2018-12-11 21:37:22 +1300

    fuck

---
+++ Meditations/Assets/GravityMod.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class GravityMod : MonoBehaviour {
+	float startGrav;
+	public GameObject killThis;
+	FirstPersonController fps;
+	public Image blackScreen;
+	bool endGame;
+	int t;
+	// Use this for initialization
+	void Start () {
+		fps = GetComponent<FirstPersonController>();
+		startGrav = fps.m_GravityMultiplier;
+	}
+	
+	void Update(){
+		if (endGame){
+			fps.m_WalkSpeed = 0;
+			if (blackScreen.color.a >= 1){
+			var tempc = blackScreen.color;
+			tempc.a = blackScreen.color.a - 0.01f;
+			blackScreen.color = tempc;
+
+			if (blackScreen.color.a <= 0){
+				t++;
+			}
+
+			if (t > 10 * Time.deltaTime){
+				Application.Quit();
+			}
+		}
+	}
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "GravityMod"){
+			fps.m_GravityMultiplier = 0.0f;
+		}
+		if (other.tag == "TerrainKill"){
+			killThis.GetComponent<TerrainCollider>().enabled = false;
+		}
+		if (other.tag == "End"){
+			endGame = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		if (other.tag == "GravityMod"){
+			fps.m_GravityMultiplier = startGrav;
+		}
+	}
+}
---
+++ Meditations/Assets/RandomEmissionColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEmissionColor : MonoBehaviour {
+	MeshRenderer[] meshRenderers;
+
+	// Use this for initialization
+	void Start () {
+		meshRenderers = GetComponentsInChildren<MeshRenderer>();
+		Color col = Random.ColorHSV(0f,1f,0.7f,1f,0.7f,1f) * Mathf.LinearToGammaSpace(3f);
+			
+		foreach (MeshRenderer m in meshRenderers){
+			m.material.SetColor("_EmissionColor", col);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
